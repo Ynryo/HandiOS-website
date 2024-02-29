3ce4e39de199ea8e86b1339138b3b621d84d7313
Ynryo <ewenatideklein@gmail.com>
2024-02-29 18:25:40 +0100

    commit

---
+++ assets/src/header.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="theme-color" content="#fff">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="author" content="Ynryo">
+<meta property="og:site_name" content="Handi'OS">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<link rel="manifest" href="/app.webmanifest">
+<link rel="icon" type="image/png" href="/assets/favicon/handios-favicon.png">
